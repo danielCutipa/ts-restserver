8e2131d75c6278eb7a958502f724d73acda78a62
Daniel Cutipa <daniel.cutipa@mensivo.com>
2021-07-04 04:11:57 -0400

    remove env configuration

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Recuerden que deben de ejecutar `npm install` para reconstruir los modulos de Node.
 
-- tambien es necesario configurar la coneccion a la base de datos como indica en el archivo .example.env
+- tambien es necesario configurar la coneccion a la base de datos en el archivo dbConnection.ts
+- ademas es necesario tener configurar un puerto en el archivo .env
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const DB_NAME = process.env.DB_NAME || '';
-const DB_USER = process.env.DB_USER || '';
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-const db = new sequelize_1.Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
+const db = new sequelize_1.Sequelize('ts-node', 'root', 'root', {
+    host: 'localhost',
     dialect: 'mysql'
     // logging: false
 });
